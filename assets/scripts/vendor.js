710740b4c04678a603817931fa71edf6465e549f
Dement057 <Dement057@bitbucket.org>
2022-03-15 15:31:29 +0300

    add file vendor.js

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -2,7 +2,7 @@ const monsterHealthBar = document.getElementById('monster-health');
 const playerHealthBar = document.getElementById('player-health');
 const bonusLifeEl = document.getElementById('bonus-life');
 
-const attackBtn = document.getElementById('attack-btn');
+const attackBtn = document.getElementById('attack-btn2');
 const strongAttackBtn = document.getElementById('strong-attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
